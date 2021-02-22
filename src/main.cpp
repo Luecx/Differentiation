f8f8ce471fd4f13e21a823d85028bdfb23774ea5
Finn Eggers <web@finneggers.de>
2021-02-22 15:14:18 +0100

    bench

--- src/main.cpp
+++ src/main.cpp
@@ -38,35 +38,46 @@ int main() {
     }
 
 //
-    for (int j = 0; j < 1; j++) {
-        auto start = std::chrono::high_resolution_clock::now();
-
-        float lossSum = 0;
-#pragma omp parallel for schedule(auto) num_threads(32) reduction(+:lossSum)
-        for (int i = 0; i < 1024*1024*16; i++) {
-
-            const int threadID = omp_get_thread_num();
-
-            l1->apply(&in, tds[threadID]);
-            l2->apply(tds[threadID]);
-            l3->apply(tds[threadID]);
-
-            lossSum += loss->backprop(dynamic_cast<Data<8> *>(tds[threadID]->output[2]), &target,
-                                      dynamic_cast<Data<8> *>(tds[threadID]->output_gradient[2]));
-            l3->backprop(tds[threadID]);
-            l2->backprop(tds[threadID]);
-            l1->backprop(&in, tds[threadID]);
 
 
+    double first_thread_time = 0;
+    for(int t = 1; t < 16; t++){
+        for (int j = 0; j < 1; j++) {
+            auto start = std::chrono::high_resolution_clock::now();
 
-        }
+            float lossSum = 0;
+#pragma omp parallel for schedule(auto) num_threads(t) reduction(+:lossSum)
+            for (int i = 0; i < 1024*1024*8; i++) {
+
+                const int threadID = omp_get_thread_num();
+
+                l1->apply(&in, tds[threadID]);
+                l2->apply(tds[threadID]);
+                l3->apply(tds[threadID]);
+
+                lossSum += loss->backprop(dynamic_cast<Data<8> *>(tds[threadID]->output[2]), &target,
+                                          dynamic_cast<Data<8> *>(tds[threadID]->output_gradient[2]));
+                l3->backprop(tds[threadID]);
+                l2->backprop(tds[threadID]);
+                l1->backprop(&in, tds[threadID]);
+
+
+            }
+
+            auto finish = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = finish - start;
+
+            double time = elapsed.count();
+            if(t == 1){
+                first_thread_time = time;
+            }
+
+            std::cout << std::left << std::setw(20) << elapsed.count() << "scaling:" << round(first_thread_time / (time * t)*100) << " %" << std::endl;
+    }
 
 
 
-        auto finish = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = finish - start;
-        std::cout << std::left << std::setw(20) << elapsed.count() << "ops:"
-                  << 1024 * 1024 * (512 * 33 / elapsed.count()) << std::endl;
+
     }
 
 
